aaf505edd41e17c2444ba708488f6689986f6d81
lckmnzans <lckmnzans.elektro@gmail.com>
2023-07-23 13:10:56 +0700

    add bottom menu with fab

--- app/src/main/java/com/simple/bottomnavwithfab/MainActivity.kt
+++ app/src/main/java/com/simple/bottomnavwithfab/MainActivity.kt
@@ -2,10 +2,18 @@ package com.simple.bottomnavwithfab
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bottomNavigationView: BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomNavigationView = findViewById(R.id.bottomNavView)
+        bottomNavigationView.apply {
+            background = null
+            menu.getItem(2).isEnabled = false
+        }
     }
 }
