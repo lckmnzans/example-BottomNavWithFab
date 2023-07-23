5ce13a4d43f249142b835510058bf4358071b027
lckmnzans <lckmnzans.elektro@gmail.com>
2023-07-23 13:48:05 +0700

    add menu action to bottom menu

--- app/src/main/java/com/simple/bottomnavwithfab/MainActivity.kt
+++ app/src/main/java/com/simple/bottomnavwithfab/MainActivity.kt
@@ -2,12 +2,13 @@ package com.simple.bottomnavwithfab
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.view.MenuItem
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationBarView
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var bottomNavigationView: BottomNavigationView
-    override fun onCreate(savedInstanceState: Bundle?) {
+class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListener {
+    private lateinit var bottomNavigationView: NavigationBarView
+        override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
@@ -19,6 +20,40 @@ class MainActivity : AppCompatActivity() {
         bottomNavigationView.apply {
             background = null
             menu.getItem(2).isEnabled = false
+        }.setOnItemSelectedListener(this)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.home -> {
+                setFragment("Home")
+                true
+            }
+            R.id.Search -> {
+                setFragment("Search")
+                true
+            }
+            R.id.Profile -> {
+                setFragment("Profile")
+                true
+            }
+            R.id.Settings -> {
+                setFragment("Settings")
+                true
+            }
+            else -> false
         }
+    }
+
+    private fun setFragment(title: String) {
+        val fragment = PageFragment()
+        val bundle = Bundle()
+        bundle.putString(PageFragment.EXTRA_TITLE, title)
+        fragment.arguments = bundle
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_view, fragment)
+            .addToBackStack(title)
+            .commit()
     }
 }
--- app/src/main/java/com/simple/bottomnavwithfab/PageFragment.kt
+++ app/src/main/java/com/simple/bottomnavwithfab/PageFragment.kt
@@ -5,11 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 
 class PageFragment : Fragment() {
+    private lateinit var tvPageTitle: TextView
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
@@ -18,6 +21,15 @@ class PageFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        tvPageTitle = view.findViewById(R.id.tv_page)
 
+        if (arguments != null) {
+            val title = arguments?.getString(EXTRA_TITLE).toString()
+            tvPageTitle.text = title
+        }
+    }
+
+    companion object {
+        const val EXTRA_TITLE = "extra_title"
     }
 }
