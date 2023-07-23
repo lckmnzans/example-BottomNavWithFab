81dc56eff772ddce3801ce8302aba2625b9aabdd
lckmnzans <lckmnzans.elektro@gmail.com>
2023-07-23 13:15:25 +0700

    add fragment

--- app/src/main/java/com/simple/bottomnavwithfab/MainActivity.kt
+++ app/src/main/java/com/simple/bottomnavwithfab/MainActivity.kt
@@ -2,6 +2,7 @@ package com.simple.bottomnavwithfab
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
@@ -9,6 +10,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction()
+            .add(R.id.fragment_view, PageFragment())
+            .commit()
 
         bottomNavigationView = findViewById(R.id.bottomNavView)
         bottomNavigationView.apply {
